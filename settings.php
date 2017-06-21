d06b904bd51274bbfc095d8484419d352913ef1d
Dmytro Chaplynskyi <chaplinsky.dmitry@gmail.com>
2017-06-21 12:13:55 +0200

    Misc fixes to make it deploy

--- settings.php
+++ settings.php
@@ -1,6 +1,6 @@
 <?php
     define("CLIENT_SECRET", '/configs/pepbot-b98084ef8d1e.json');
     define("SPREADSHEET_ID", '1BkTms98hmbU8nSz7HtpDPrriFV5cSp3Z-3i_UBft0Rs');
-    define("DEBUG", true);
+    define("DEBUG", false);
     define("STATIC_FOLDER", "/static");
     define("CACHE_TIME", 3600 * 4);
